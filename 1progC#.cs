0f57007dabe612e950e48a8ad55fb1bf18295669
Victor Monteiro <devvictor.io@gmail.com>
2024-04-30 13:32:09 -0300

    first C# commit

---
+++ 1progC#.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+        
+        Console.WriteLine($"Olá, {nome}! Bem-vindo ao mundo da programação com C#!");
+    }
+}
