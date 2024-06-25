a2736462a1f9b6c3079dc8f82f5691282f5ab1af
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-06-25 14:22:12 -0300

    5th commit
    Boolean type content

--- Program.cs
+++ Program.cs
@@ -7,23 +7,16 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            var num = 2.5; // double até 64 bits
-            double num1 = 2.5; // double até 64 bits
+            bool usuarioJaCadastrado = false; // boolean type T or F
+            var usuario2 = true; // a partir do momneto que defininir como F ou T se torna um boolean type
 
-            var num3 = 2500.00f; // 32 bits
-            float num2 = 2.500f; // para utilizar o float é necessário colocar a notação f
+            bool pagamentorecebido = true;
+            var batatadoceEhbom = false;
 
-            var num4 = 2500.0m; // 128 bits
-            decimal num5 = 2500.000m; // para utilizar o decimal é necessário colocar a notação m
-
-
-            Console.WriteLine(num);
-            Console.WriteLine(num1);
-            Console.WriteLine(num2);
-            Console.WriteLine(num3);
-            Console.WriteLine(num4);
-            Console.WriteLine(num5);
-
+            Console.WriteLine(usuarioJaCadastrado);
+            Console.WriteLine(usuario2);
+            Console.WriteLine(pagamentorecebido);
+            Console.WriteLine(batatadoceEhbom);
 
         }
     }
