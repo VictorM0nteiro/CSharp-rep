c9ca0c54cdcea08a6300d8cd4b9eaab246ca7a15
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-06-25 13:27:46 -0300

    2nd commit
    Studying about bytes and sbytes

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@
 
 namespace MeuApp
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            string texto = "Testando";
-            Console.WriteLine(texto);
+            sbyte meuSbyte = -128;
+            Console.WriteLine(meuSbyte);
+
         }
     }
 }
