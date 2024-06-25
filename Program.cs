ae32924ce06086263ccb5a0658474f9cf05a25cb
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-06-25 13:53:17 -0300

    third commit
    Studying about  INT types

--- Program.cs
+++ Program.cs
@@ -7,8 +7,23 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            sbyte meuSbyte = -128;
-            Console.WriteLine(meuSbyte);
+            short num = -10; //pode numeros positivos e negativos até 16 bits
+            ushort num2 = 10; // pode somente numeros positivos até 16 bits
+
+            int num3 = -1000000000; // pode numeros positivos e negativos até 32 bits
+            uint num4 = 1000000000; // pode somente numeros positivos até 32 bits
+
+            long num5 = -1000000000000000000; // pode numeros positivos e negativos até 64 bits
+            ulong num6 = 10000000000000000000; // pode somente numeros positivos até 64 bits
+
+
+            Console.WriteLine(num);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
+            Console.WriteLine(num5);
+            Console.WriteLine(num6);
+
 
         }
     }
