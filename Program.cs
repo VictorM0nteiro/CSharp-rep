2fe6f063a4dde2938a0341183447ad06f339367d
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-06-27 10:28:21 -0300

    9th commit - object type
    Undestanding the object type

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace MeuApp
 {
@@ -8,14 +9,22 @@ namespace MeuApp
         static void Main(string[] args)
         {   
 
+            // var quantidade = ?? // Para situações em que não saberei o tipo utilizar o OBJECT
+            object quantidade = 2.5;
+            quantidade = 3; // posso ficar coinvertendo sem nenhum problema double -> int
+            quantidade = 5.5; // e vice versa também // basicamente ele pode ser qualquer coisa;
 
-            var idade = 25; //int
-            idade = 26;
-            //idade = "Victor"; Não posso implicitamente mudar algo que foi definido como INT para uma string
-            var letra = 'b'; // haverá situações que criaremos um tipo complexo e o VAR economizará a quantidade de código;
+            object teste = "bom";
+            teste = 'b'; // não limita strings -> chars e vice versa
 
-            Console.WriteLine(idade);
-            Console.WriteLine(letra);
+            object teste2 = "good";
+            teste2 = 'c';
+            teste2 = 5555555555555555555; // posso converter em ints e double, floats etc;
+            
+            
+            Console.WriteLine(quantidade);
+            Console.WriteLine(teste);
+            Console.WriteLine(teste2);
 
 
 
