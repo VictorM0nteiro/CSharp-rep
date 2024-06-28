fd6bf6fb6233181fe1011d5313586f581e3fe572
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-06-28 10:07:33 -0300

    14th commit - Explicit conversion
    Understanding how to use explicit conversion

--- Program.cs
+++ Program.cs
@@ -7,43 +7,12 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            float valor = 25.8f;
-            int outro = 25;
-            valor = outro; // Conversão implícita / implicit conversion, basicamente declaramos dois tipos e depois os associamos;
-            Console.WriteLine(valor);
-            
-
-            // Conversão implícita só funciona se os tipos forem compatíveis, portanto abaixo tem uma tabela com as compatibilidades;
-
-            // - sbyte 
-            // - short, int, long, float, double, decimal
-
-            // - byte
-            // - short, ushort, int, uint, long, ulong, float, double, decimal
-            
-            // - short
-            // - int, long, float, double, or decimal
-            
-            // - ushort
-            // - int, uint, long, ulong, float, double, or decimal
-            
-            // - int
-            // - long, float, double, or decimal |
-
-            // - uint
-            // - long, ulong, float, double, or decimal
-
-            // - long
-            // - float, double, or decimal
-
-            // - ulong
-            // ~ float, double, or decimal
-            
-            // - char |
-            // - ushort, int, uint, long, ulong, float, double, or decimal
-
-            // - float
-            // - double |
+            int num1 = 10;
+            int num2 = num1*10;
+            float num3 = (float)num2/3; // Explicit conversion, is necessary to declare the type before, in order to convert non-compatible types;
+            double num4 = (double)num3/7;
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
         }
 
     }
