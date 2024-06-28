56b2aa9dc6450e8bd614a4b64c0f7e175a9f5d48
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-06-28 09:51:59 -0300

    13th commit - implicit conversion
    Understanding about implicit conversion

--- Program.cs
+++ Program.cs
@@ -7,12 +7,43 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-             int banana; // default value => 0;
-             float ban; // default value => 0;
-             decimal ttt; // default value => 0;
-             bool torf; // default value => false;
-             char mmmm; // default value => '\0';
-             string cin; // default value => "";
+            float valor = 25.8f;
+            int outro = 25;
+            valor = outro; // Conversão implícita / implicit conversion, basicamente declaramos dois tipos e depois os associamos;
+            Console.WriteLine(valor);
+            
+
+            // Conversão implícita só funciona se os tipos forem compatíveis, portanto abaixo tem uma tabela com as compatibilidades;
+
+            // - sbyte 
+            // - short, int, long, float, double, decimal
+
+            // - byte
+            // - short, ushort, int, uint, long, ulong, float, double, decimal
+            
+            // - short
+            // - int, long, float, double, or decimal
+            
+            // - ushort
+            // - int, uint, long, ulong, float, double, or decimal
+            
+            // - int
+            // - long, float, double, or decimal |
+
+            // - uint
+            // - long, ulong, float, double, or decimal
+
+            // - long
+            // - float, double, or decimal
+
+            // - ulong
+            // ~ float, double, or decimal
+            
+            // - char |
+            // - ushort, int, uint, long, ulong, float, double, or decimal
+
+            // - float
+            // - double |
         }
 
     }
