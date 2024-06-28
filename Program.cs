18ba975772a5b65cac0b16a9bb5211e4bae1ca3d
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-06-28 09:23:55 -0300

    12th commit - types default values
    Each type has its own deafuylt value (when i do not declare a value for the type), here i put a few examples;

--- Program.cs
+++ Program.cs
@@ -1,29 +1,19 @@
 ﻿using System;
-using System.Globalization;
 
 namespace MeuApp
 {
-    
+
     class Program
     {
         static void Main(string[] args)
-        {   
-             System.String nome = "Victor"; // original type
-             string nome2 = "Victor"; // type alias, na prática são a mesma coisa;
- 
-             int idade = 18; // Alias
-             Int32 idade2 = 18; // Original type
-
-             Int128 num = 10000000000000000000; // alias;
-
-             Console.WriteLine(nome);
-             Console.WriteLine(nome2);
-
-             Console.WriteLine(idade);
-             Console.WriteLine(idade2);
-
-             Console.WriteLine(num);
+        {
+             int banana; // default value => 0;
+             float ban; // default value => 0;
+             decimal ttt; // default value => 0;
+             bool torf; // default value => false;
+             char mmmm; // default value => '\0';
+             string cin; // default value => "";
         }
-        
+
     }
 }
