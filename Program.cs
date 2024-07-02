a1274e724cfdcf3414d4da35075988c46dabe162
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-07-02 14:20:39 -0300

    17th commit - arithmetic operators
    Something i was already using before this file, i am doing this so that i can have everything saved in one place

--- Program.cs
+++ Program.cs
@@ -7,30 +7,15 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            // Convertendo uma string em um inteiro
-            string numeroString = "123";
-            int numeroInteiro = Convert.ToInt32(numeroString); //Converte a string "123" para o inteiro 123
-            Console.WriteLine(numeroInteiro); //Saida: 123;
+            int soma = 25+22;
+            int subtracao = 25 -22;
+            int mult = 25*22;
+            int div =  22 / 5; // Não é o caso mas a ordem de precêdencia é mult e div para depois sub e som
 
-            // Convertendo um double em um inteiro
-            double numerodouble = 123.45;
-            int numeroInteiro2 = Convert.ToInt32(numerodouble); // Converte o double 123.45 para o inteiro 123 (arredonda para o valor inteiro mais próximo)
-            Console.WriteLine(numeroInteiro2); // Saída: 123
-
-            //Convertendo um booleano em uma string
-            bool verdadeiroFalso = true;
-            string booleanoString = Convert.ToString(verdadeiroFalso); // Converte o booleano true para a string "True"
-            Console.WriteLine(booleanoString); // Saída: True
-
-            // Convertendo uma string em um datetime
-            string dataString = "2024-07-01  19:14:07";
-            DateTime data = Convert.ToDateTime(dataString); // Converte a string "2024-07-01" para um DateTime
-            Console.WriteLine(data); // Saída: 01/07/2024 00:00:00
-
-            //Convertendo um inteiro em uma String 
-            int numero = 456;
-            string numstring = Convert.ToString(numero); // Converte o inteiro 456 para a string "456"
-            Console.WriteLine(numstring); // Saída: 456
+            Console.WriteLine(soma);
+            Console.WriteLine(subtracao);
+            Console.WriteLine(mult);
+            Console.WriteLine(div); // A few examples of arithmetics operators
         }
 
     }
