b33077a6dd09ef6d4f9a13571c992dc978d7eb79
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-07-02 14:55:16 -0300

    19th commit - + exemples
    More exemples of what i've learned today;

--- Program.cs
+++ Program.cs
@@ -7,16 +7,41 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-             int x = 0; // atribuição;
-             x += 5; // x = x + 5;
-             x -= 1; // x = x-1;
-             x *= 10; // x = x*10;
-             x /= 2; // x = x/2;
-             int y = 0;
-             y++; // Soma 1
+             int x = 2;
+             x = 2+2/2;
+            Console.WriteLine(x);
+            
+            int y = 75/23;
+            Console.WriteLine(y); // Como é um inteiro o resultado será arredondado;
 
-             Console.WriteLine(x); // Resultado de todas as atribuições sucessivas anteriores
-             Console.WriteLine(y); // resultado 1;
+            double z = 75/23.0; // importante esse ), pq se não a conta é tratada como int
+            Console.WriteLine(z); // Não erredondaria porque é real
+            
+
+
+            int d = 2;
+            d += 3; // Somando o valor d com 3; aldo assim d+3 não funcionaria, somente dentro de outra variável
+            Console.WriteLine(d); 
+
+            int r = 2;
+            r -= 3;
+            Console.WriteLine(r);
+
+            int p = 7;
+            p++; // Incrementa o Valor em 1; Seria o contrário se fosse p--;
+            Console.WriteLine(p);
+
+
+
+            //Comparções
+            int c = 2;
+            Console.WriteLine(x == 25); // bool int.operator ==(int left, int right), o resultado disso sera um booleano que no caso é falso
+
+            int t = 25;
+            Console.WriteLine(t <= 100); // resulta em um bool true
+
+            int m = 2; 
+            Console.WriteLine(m != 25);
         }
 
     }
