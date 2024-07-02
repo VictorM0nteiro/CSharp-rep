7765d8ef6b9fa9fc719846900c76416d4e431a0c
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-07-02 14:30:21 -0300

    18th commit - atribution operators
    Undestanding their basic usages;

--- Program.cs
+++ Program.cs
@@ -7,15 +7,16 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-            int soma = 25+22;
-            int subtracao = 25 -22;
-            int mult = 25*22;
-            int div =  22 / 5; // Não é o caso mas a ordem de precêdencia é mult e div para depois sub e som
+             int x = 0; // atribuição;
+             x += 5; // x = x + 5;
+             x -= 1; // x = x-1;
+             x *= 10; // x = x*10;
+             x /= 2; // x = x/2;
+             int y = 0;
+             y++; // Soma 1
 
-            Console.WriteLine(soma);
-            Console.WriteLine(subtracao);
-            Console.WriteLine(mult);
-            Console.WriteLine(div); // A few examples of arithmetics operators
+             Console.WriteLine(x); // Resultado de todas as atribuições sucessivas anteriores
+             Console.WriteLine(y); // resultado 1;
         }
 
     }
