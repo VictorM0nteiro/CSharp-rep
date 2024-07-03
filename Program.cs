ef6468d547df1d67fea93126b187c3fea69b44a7
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-07-03 14:35:50 -0300

    20th commit - Logic ops
    Logic operators and a few exemples

--- Program.cs
+++ Program.cs
@@ -7,41 +7,23 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
-             int x = 2;
-             x = 2+2/2;
-            Console.WriteLine(x);
-            
-            int y = 75/23;
-            Console.WriteLine(y); // Como é um inteiro o resultado será arredondado;
+              // logic operators
+              int x = 5;
+              int y = 12;
+              Console.WriteLine(( x > 25) && (y > 10) ); // && os dois tem que ser T para a saida ser T
+              Console.WriteLine((x > 25) || (y > 10)); // || somente um tem que ser T para o resultado ser T
+              Console.WriteLine(!(x > 25)); // Inverte o resultado, "não é maior que || não é menor que";
 
-            double z = 75/23.0; // importante esse ), pq se não a conta é tratada como int
-            Console.WriteLine(z); // Não erredondaria porque é real
-            
+              Console.WriteLine(" ");
 
+              int a = 12;
+              bool e = (a > 25) && (a < 40); // False
+              bool ou = ( a > 25) || ( a < 40); // True
+              bool neg = !(x < 25); // False
 
-            int d = 2;
-            d += 3; // Somando o valor d com 3; aldo assim d+3 não funcionaria, somente dentro de outra variável
-            Console.WriteLine(d); 
-
-            int r = 2;
-            r -= 3;
-            Console.WriteLine(r);
-
-            int p = 7;
-            p++; // Incrementa o Valor em 1; Seria o contrário se fosse p--;
-            Console.WriteLine(p);
-
-
-
-            //Comparções
-            int c = 2;
-            Console.WriteLine(x == 25); // bool int.operator ==(int left, int right), o resultado disso sera um booleano que no caso é falso
-
-            int t = 25;
-            Console.WriteLine(t <= 100); // resulta em um bool true
-
-            int m = 2; 
-            Console.WriteLine(m != 25);
+              Console.WriteLine(e);
+              Console.WriteLine(ou);
+              Console.WriteLine(neg);
         }
 
     }
