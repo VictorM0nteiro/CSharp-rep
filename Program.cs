8f8aef9d177bc702f976104cd31ddf0e76729d28
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-07-04 10:21:35 -0300

    23rd commit - exercise with if/else
    Solved an exercise using if/elseif/else, hte exercise was this one https://judge.beecrowd.com/pt/problems/view/1037

--- Program.cs
+++ Program.cs
@@ -9,15 +9,18 @@ namespace MeuApp
         {
             Console.Write("");
             string num = Console.ReadLine();
-            int x = Convert.ToInt32(num);
+            double x = Convert.ToDouble(num);
 
-            Console.Write("");
-            string num2 = Console.ReadLine();
-            int y = Convert.ToInt32(num2);
-            
-            int soma = x + y;
-            Console.WriteLine("X = " + soma);
-
+            if( x >= 0 && x <= 25.00)
+                Console.WriteLine("Intervalo [0,25]");
+            else if( x > 25.00 && x <= 50.00)
+                Console.WriteLine("Intervalo (25,50]");
+            else if( x > 50.00 && x <= 75.00)
+                Console.WriteLine("Intervalo (50,75]");
+            else if( x > 75.00 && x <= 100.00)
+                Console.WriteLine("Intervalo (75,100]");
+            else
+                Console.WriteLine("Fora de intervalo");
         }
 
     }
