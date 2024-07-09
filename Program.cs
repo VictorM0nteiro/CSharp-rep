abb38c18d2a526c018449f79ef29b49bc85893ee
Victor Hugo Monteiro da silva <116459509+VictorM0nteiro@users.noreply.github.com>
2024-07-09 10:29:33 -0300

    27th commit - do/while
    Repetition loop do/while

--- Program.cs
+++ Program.cs
@@ -12,56 +12,20 @@ namespace MeuApp
             int x = Convert.ToInt32(num);
 
             int i = 0;
-            while( i <= x){
+            do{
                 Console.WriteLine(i);
                 i++;
             }
+            while(i <= x);
             Console.WriteLine(" ");
 
-            //Conta de 0 a 5
-            var j = 0;
-            while( j <= 5){
-                Console.WriteLine(j);
-                j++;
-            }
-            Console.WriteLine(" ");
-
-            //Conta de 1 a 5
-            var a = 1;
-            while(a <= 5){
-                Console.WriteLine(a);
-                a++;
-            }
-            Console.WriteLine(" ");
-
-            //Conta de 0 a 4
-            var n = 0;
-            while(n < 5){
-                Console.WriteLine(n);
-                n++;
-            }
-            Console.WriteLine(" ");
-
-            //Conta de 5 a 0
-            var q = 5;
-            while(q >= 0){
-                Console.WriteLine(q);
-                q--;
-            }
-
-            //INcrementando antes 1 a 6
-            var w = 0;
-            while(w <= 5){
-                w++;
-                Console.WriteLine(w);
-            }
-
-            // //infinito 
+            // infinito
             // var valor = 0;
-            // while(true){ // Infinito pois true nunca sera false
-            //     valor++;
+            // do{
             //     Console.WriteLine(valor);
+            //     valor++;
             // }
+            // while(true);
         }
 
     }
